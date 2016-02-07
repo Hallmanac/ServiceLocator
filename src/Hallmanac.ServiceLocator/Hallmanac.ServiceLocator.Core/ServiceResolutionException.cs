45457daeee6ba1c6c3447616e7b4f24b9556a471
Brian Hall <brian@nebbiatech.com>
2016-02-07 15:51:09 -0500

    Initial Commit of Hallmanac.ServiceLocator Solution.

---
+++ src/Hallmanac.ServiceLocator/Hallmanac.ServiceLocator.Core/ServiceResolutionException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace Hallmanac.ServiceLocator.Core
+{
+    [Serializable]
+    public class ServiceResolutionException : Exception
+    {
+        public ServiceResolutionException(Type service)
+            :
+                base($"Could not resolve serviceType '{service}'")
+        {
+            ServiceType = service;
+        }
+
+
+        public ServiceResolutionException(Type service, Exception innerException)
+            : base($"Could not resolve serviceType '{service}'", innerException)
+        {
+            ServiceType = service;
+        }
+
+
+        public ServiceResolutionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+
+        public Type ServiceType { get; set; }
+    }
+}
