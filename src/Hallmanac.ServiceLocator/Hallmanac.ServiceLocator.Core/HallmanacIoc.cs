8917b49894cd69a5d561f8f522514129fa028575
Brian Hall <brian@nebbiatech.com>
2016-02-07 20:54:01 -0500

    Added documentation comments.

--- src/Hallmanac.ServiceLocator/Hallmanac.ServiceLocator.Core/HallmanacIoc.cs
+++ src/Hallmanac.ServiceLocator/Hallmanac.ServiceLocator.Core/HallmanacIoc.cs
@@ -3,11 +3,20 @@
 
 namespace Hallmanac.ServiceLocator.Core
 {
+    /// <summary>
+    /// Class container for managing the IOC container for the application during runtime.
+    /// </summary>
     public class HallmanacIoc
     {
+        /// <summary>
+        /// Static singleton that returns the current IOC container for the application.
+        /// </summary>
         public static IServiceLocator Current { get; set; }
 
 
+        /// <summary>
+        /// Sets the service Locator for the appliction. This is typically called during an application startup process of some kind.
+        /// </summary>
         public static void SetServiceLocator(Func<IServiceLocator> create)
         {
             if (create == null)
